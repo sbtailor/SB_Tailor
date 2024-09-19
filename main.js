90934fdd450b7a068c75620e8954043afb613447
sbtailor <sbtailor90@gmail.com>
2024-09-18 22:45:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let toggle = document.querySelector('.toggle');
+let sidebar = document.querySelector('.sidebar');
+
+toggle.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar2')
+})
